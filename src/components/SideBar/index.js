61c484bae53db3bf983f1b54a227e9126b0dabbf
yamnyk <notAunicorN2018>
2020-06-05 00:58:04 +0300

    add fixing of active sidebar item

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import {Link} from "react-router-dom";
-import styles from './SideBar.module.scss'
+import styles from './SideBar.module.scss';
+import {useLocation} from 'react-router-dom';
 
 const SideBar = props => {
   const {items} = props,
-    IconWrap = props => <div className={styles.SideBar__Icon}>{props.children}</div>;
+    IconWrap = props => <div className={styles.SideBar__Icon}>{props.children}</div>,
+    location = useLocation();
   
-  const listItems = items.map(item => (
-      <li key={item.id} className={styles.SideBar__Item}>
-        <IconWrap>{item.icon}</IconWrap>
-        <Link to={item.linkURL} className={styles.SideBar__Link}>{item.text}</Link>
-      </li>
-    )
+  const listItems = items.map(item => {
+      let classList = [styles.SideBar__Item];
+      
+      if (item.linkURL === location.pathname) {
+        classList.push(styles.SideBar__Item_Active)
+      }
+      
+      classList = classList.join(' ');
+      
+      return (
+        <li key={item.id} className={classList}>
+          <IconWrap>{item.icon}</IconWrap>
+          <Link to={item.linkURL} className={styles.SideBar__Link}>{item.text}</Link>
+        </li>
+      )
+    }
   );
   return (
     <div className={styles.SideBar}>
