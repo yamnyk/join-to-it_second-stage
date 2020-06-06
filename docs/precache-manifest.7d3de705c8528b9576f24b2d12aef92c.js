73ca7edd5027b445ef4c5bc6ab1726875746c6ba
yamnyk <notAunicorN2018>
2020-06-06 16:55:47 +0300

    add prod code to git

---
+++ docs/precache-manifest.7d3de705c8528b9576f24b2d12aef92c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96a90a43f65f30abc50cac89f800722e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e37a1f506df5612b8a0a",
+    "url": "/static/css/main.a1204ea2.chunk.css"
+  },
+  {
+    "revision": "63e74eac922bd5be550b",
+    "url": "/static/js/2.605220fb.chunk.js"
+  },
+  {
+    "revision": "a13369e7405157f254f1ffcc4cc651e5",
+    "url": "/static/js/2.605220fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e37a1f506df5612b8a0a",
+    "url": "/static/js/main.80392380.chunk.js"
+  },
+  {
+    "revision": "0cd84c609d64f7a5e33e",
+    "url": "/static/js/runtime-main.7a764257.js"
+  }
+]);
